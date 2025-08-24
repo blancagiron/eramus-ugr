ec2586de6587bd2e77303ec28fa21b7f07b4c1f0
blancagiron <blancagiron@correo.ugr.es>
2025-08-24 20:07:39 +0200

    correccion de error

--- backend/db.py
+++ backend/db.py
@@ -1,4 +1,4 @@
 from pymongo import MongoClient
 
 client = MongoClient("mongodb://root:example@localhost:27017")
-db = client["erasmus"]  # o "erasmus-ugr" si así la llamaste
+db = client["erasmus"]  
